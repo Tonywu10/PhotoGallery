c680f1f27811c1c5e7eaeb8ef422b072dd116179
Tonywu10 <tonywu1061@gmail.com>
2016-11-06 11:23:21 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tonywu10/photogallery/NotificationReceiver.java
@@ -0,0 +1,28 @@
+package com.example.tonywu10.photogallery;
+
+import android.app.Activity;
+import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationManagerCompat;
+import android.util.Log;
+
+/**
+ * Created by tonywu10 on 2016/11/6.
+ */
+
+public class NotificationReceiver extends BroadcastReceiver{
+    private static final String TAG="NotificationReceiver";
+    @Override
+    public void onReceive(Context c, Intent i) {
+        Log.i(TAG,"received result: "+getResultCode());
+        if(getResultCode()!= Activity.RESULT_OK){
+            return;
+        }
+        int requestCode=i.getIntExtra(PollService.REQUEST_CODE,0);
+        Notification notification=i.getParcelableExtra(PollService.NOTIFICATION);
+        NotificationManagerCompat notificationManager=NotificationManagerCompat.from(c);
+        notificationManager.notify(requestCode,notification);
+    }
+}
